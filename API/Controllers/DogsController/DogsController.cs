58864da27592007f79c285f7a7d55a0d9420f788
Filip <filip.trajanovski@student.nbi-handelsakademin.se>
2023-12-11 08:45:15 +0100

    Validering på DogsController

--- API/Controllers/DogsController/DogsController.cs
+++ API/Controllers/DogsController/DogsController.cs
@@ -7,6 +7,7 @@ using Application.Queries.Dogs.GetAll;
 using Application.Queries.Dogs.GetById;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Application.Validators.Dog;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,11 +18,13 @@ namespace API.Controllers.DogsController
     public class DogsController : ControllerBase
     {
         internal readonly IMediator _mediator;
+        internal readonly DogValidator _dogValidator;
 
         // Konstruktor som tar in en instans av IMediator (MediatR används för att implementera CQRS-mönstret)
-        public DogsController(IMediator mediator)
+        public DogsController(IMediator mediator, DogValidator dogValidator)
         {
             _mediator = mediator;
+            _dogValidator = dogValidator;
         }
 
         // // Hämta alla hundar från databasen
@@ -29,8 +32,19 @@ namespace API.Controllers.DogsController
         [Route("getAllDogs")]
         public async Task<IActionResult> GetAllDogs()
         {
-            return Ok(await _mediator.Send(new GetAllDogsQuery()));
-            //return Ok("GET ALL DOGS");
+            try
+            {
+                // Försök hämta alla hundar från databasen
+                var dogs = await _mediator.Send(new GetAllDogsQuery());
+
+                // Returnera hundarna om hämtningen lyckades
+                return Ok(dogs);
+            }
+            catch (Exception ex)
+            {
+                // Om det uppstår ett fel, returnera ett felmeddelande
+                return BadRequest($"Error getting dogs: {ex.Message}");
+            }
         }
 
         //  // Hämta en hund med ett specifikt ID
@@ -38,7 +52,34 @@ namespace API.Controllers.DogsController
         [Route("getDogById/{dogId}")]
         public async Task<IActionResult> GetDogById(Guid dogId)
         {
-            return Ok(await _mediator.Send(new GetDogByIdQuery(dogId)));
+            // Validera Dog Id
+            var validatedDogId = new GuidValidator().Validate(dogId);
+
+            // Felhantering för ogiltigt Dog Id
+            if (!validatedDogId.IsValid)
+            {
+                return BadRequest(validatedDogId.Errors.Select(error => error.ErrorMessage));
+            }
+
+            // Försök hämta hunden med det specifika ID:et
+            try
+            {
+                var dog = await _mediator.Send(new GetDogByIdQuery(dogId));
+
+                // Om hunden inte finns, returnera NotFound
+                if (dog == null)
+                {
+                    return NotFound("Dog not found.");
+                }
+
+                // Returnera hunden om hämtningen lyckades
+                return Ok(dog);
+            }
+            catch (Exception ex)
+            {
+                // Felhantering med ett generiskt felmeddelande
+                return BadRequest($"Error getting dog: {ex.Message}");
+            }
         }
 
         // // Skapa en ny hund
@@ -46,7 +87,25 @@ namespace API.Controllers.DogsController
         [Route("addNewDog")]
         public async Task<IActionResult> AddDog([FromBody] DogDto newDog)
         {
-            return Ok(await _mediator.Send(new AddDogCommand(newDog)));
+            //Validate Dog
+            var validatedDog = _dogValidator.Validate(newDog);
+            
+            //Error handling
+            if (!validatedDog.IsValid)
+            {
+                return BadRequest(validatedDog.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+            
+            //Try Catch
+            try
+            {
+                return Ok(await _mediator.Send(new AddDogCommand(newDog)));
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+           
         }
 
         // Update a specific dog
@@ -54,16 +113,39 @@ namespace API.Controllers.DogsController
         [Route("updateDog/{dogId}")]
         public async Task<IActionResult> UpdateDog(Guid dogId, [FromBody] DogDto updatedDog)
         {
-            var command = new UpdateDogByIdCommand(updatedDog, dogId);
-            var result = await _mediator.Send(command);
-
-            if (result != null)
+            // Validate Dog ID
+            var validatedDogId = new GuidValidator().Validate(dogId);
+            if (!validatedDogId.IsValid)
             {
-                return Ok(result);
+                return BadRequest(validatedDogId.Errors.ConvertAll(errors => errors.ErrorMessage));
             }
-            else
+
+            // Validate Updated Dog
+            var validatedUpdatedDog = _dogValidator.Validate(updatedDog);
+            if (!validatedUpdatedDog.IsValid)
             {
-                return NotFound("Dog not found.");
+                return BadRequest(validatedUpdatedDog.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            // Try Catch
+            try
+            {
+                var command = new UpdateDogByIdCommand(updatedDog, dogId);
+                var result = await _mediator.Send(command);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound("Dog not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Här kan du använda ditt eget felmeddelande för exceptions om du vill
+                return BadRequest($"Error updating dog: {ex.Message}");
             }
         }
 
@@ -72,6 +154,17 @@ namespace API.Controllers.DogsController
         [Route("deleteDog/{dogId}")]
         public async Task<IActionResult> DeleteDog(Guid dogId)
         {
+            var validatedDogId = new GuidValidator().Validate(dogId);
+            if (!validatedDogId.IsValid)
+            {
+                foreach (var error in validatedDogId.Errors)
+                {
+                    ModelState.AddModelError(error.ErrorCode, error.ErrorMessage);
+                }
+
+                return BadRequest(ModelState);
+            }
+
             var success = await _mediator.Send(new DeleteDogCommand(dogId));
 
             if (success)
